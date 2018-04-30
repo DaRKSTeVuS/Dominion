aa68f87fb46992e2fc3885f7c79ff4f59aeec9cb
Stéphane COMombés <darkstevus@gmail.com>
2018-04-30 18:33:20 +0200

    Finalisation de spy.java

--- dominion/card/base/Spy.java
+++ dominion/card/base/Spy.java
@@ -1,5 +1,7 @@
 package dominion.card.base;
 
+import java.util.*;
+
 import dominion.*;
 import dominion.card.*;
 
@@ -21,14 +23,48 @@ public class Spy extends AttackCard {
 
 	@Override
 	public void play(Player p) {
+		// On crée une liste de choix
+		List<String> choicesYN = Arrays.asList("y", "n");
 		// +1 Actions
 		p.incrementActions(1);
 		// +1 Carte => pioche 1 carte
 		p.drawCard();
-		
-		// TODO : tous les joueurs (vous aussi) dévoilent la première carte 
-		// de leur deck. Vous décidez ensuite si chaque carte dévoilée 
-		// est défaussée ou replacée sur son deck.
-			
+		// Le joueur jouant la carte dévoile la premiere carte de son deck
+		Card pioche = p.drawCard();
+		// Si le joueur à bien piocher une carte
+		if (pioche != null) {
+			// Il choisi s'il la defausse ou non
+			String input = p.choose("Voulez vous defaussé la carte " + pioche.getName() + "de votre main ? (y/n)", choicesYN, true);
+			// Si oui
+			if (input.equals("y")) {
+				// On la met dans la defausse
+				p.defausse(pioche);
+			} else {
+				// On la remet en haut de la pioche
+				p.putOnTopDraw(pioche);
+			}
+		}
+		// On parcours ses adversaire
+		for (Player op : p.otherPlayers()) {
+			// Si le joueur n'as pas de carte Action Réaction
+			if(!this.otherPlayerGotReaction(op)) {
+				// On dévoile la premiere carte du deck de {@code op}
+				pioche = op.drawCard();
+				// Le joueur {@code p} choisi s'il cette carte est defaussé ou non
+				// Si le joueur à bien piocher une carte
+				if (pioche != null) {
+					// Il choisi s'il la defausse ou non
+					String input = p.choose("Voulez vous defaussé la carte " + pioche.getName() + "de la main de" + op.getName() + " ? (y/n)", choicesYN, true);
+					// Si oui
+					if (input.equals("y")) {
+						// On la met dans la defausse
+						op.defausse(pioche);
+					} else {
+						// On la remet en haut de la pioche
+						op.putOnTopDraw(pioche);
+					}
+				}
+			}
+		}
 	}
 }
