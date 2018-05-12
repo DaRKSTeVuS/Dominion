1c136a298edda0caf2c750df0ffa648bf7cbaf78
Frimirno <32564894+Frimirno@users.noreply.github.com>
2018-05-12 19:44:01 +0200

    gné

--- dominion/card/base/Thief.java
+++ dominion/card/base/Thief.java
@@ -52,55 +52,55 @@ public class Thief extends AttackCard {
 					System.out.println("Il n'y a pas de TreasureCard à voler !");
 				}else {
 					// Si couple ne contient qu'un élement
-					//if(couple.size() == 1) {
-					System.out.println("un seul treasure, on l'ajoute a totrash");
-					// On l'ajoute a toTrash
-					toTrash.add(couple.get(0));
-					// Sinon, si couple en contien 2
-					/*	}else if(couple.size() == 2) {
+					if(couple.size() == 1) {
+						System.out.println("un seul treasure, on l'ajoute a totrash");
+						// On l'ajoute a toTrash
+						toTrash.add(couple.get(0));
+						// Sinon, si couple en contien 2
+					}else if(couple.size() == 2) {
 						System.out.println("2 treasures, on demande");
 						// On en choisit 1
 						String inputt = p.chooseCard("Choisissez une carte", couple, false);
 						// On l'ajoute à la liste
 						toTrash.add(couple.getCard(inputt));
 					}
-				}*/
+
 				}
-				// Parmi la liste des cartes à écarter, on choisit celle qu'on veut
-				// recevoir
-				// On cré un booléen indiquant si le joueur souhaite continuer à
-				// recevoir des cartes
-				boolean choice = true;
-				// Création de la liste de choix oui/non
-				List<String> choices = Arrays.asList("y", "n");
-				// Tant que la liste n'est pas vide et que l'on souhaite continuer à
-				// recevoir des cartes
-				while (toTrash.size() > 0 && choice == true) {
-					String input = p.choose(
-							"Voulez-vous défausser une carte ? (y/n)", choices,
-							false);
-					// Si on décide de défausser une carte
-					if (input.equals("y")) {
-						String inputc = p.chooseCard("Choisissez une carte à recevoir", toTrash, true);
-						// Si le joueur n'a pas choisit une carte de la liste
-						if (inputc.equals("")) {
-							// On quite la boucle
-							choice = false;
-						} else {
-							// Sinon, la carte est reçue
-							p.gain(toTrash.getCard(inputc));
-							// Et retirée de la liste toTrash
-							toTrash.remove(inputc);
-						}
-					} else {
-						// Sinon on quite la boucle
+			}
+			// Parmi la liste des cartes à écarter, on choisit celle qu'on veut
+			// recevoir
+			// On cré un booléen indiquant si le joueur souhaite continuer à
+			// recevoir des cartes
+			boolean choice = true;
+			// Création de la liste de choix oui/non
+			List<String> choices = Arrays.asList("y", "n");
+			// Tant que la liste n'est pas vide et que l'on souhaite continuer à
+			// recevoir des cartes
+			while (toTrash.size() > 0 && choice == true) {
+				String input = p.choose(
+						"Voulez-vous défausser une carte ? (y/n)", choices,
+						false);
+				// Si on décide de défausser une carte
+				if (input.equals("y")) {
+					String inputc = p.chooseCard("Choisissez une carte à recevoir", toTrash, true);
+					// Si le joueur n'a pas choisit une carte de la liste
+					if (inputc.equals("")) {
+						// On quite la boucle
 						choice = false;
+					} else {
+						// Sinon, la carte est reçue
+						p.gain(toTrash.getCard(inputc));
+						// Et retirée de la liste toTrash
+						toTrash.remove(inputc);
 					}
+				} else {
+					// Sinon on quite la boucle
+					choice = false;
 				}
-				// On écarte le reste
-				while (!toTrash.isEmpty()) {
-					toTrash.remove(0);
-				}
+			}
+			// On écarte le reste
+			while (!toTrash.isEmpty()) {
+				toTrash.remove(0);
 			}
 		}
 	}
